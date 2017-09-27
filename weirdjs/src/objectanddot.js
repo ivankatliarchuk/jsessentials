6a5eb90603895464180a810594d265225e2682f6
Ivan Katliarchuk <tanzwud.work@gmail.com>
2017-09-27 08:45:12 +0100

    functions

--- weirdjs/src/objectanddot.js
+++ weirdjs/src/objectanddot.js
@@ -5,3 +5,22 @@ var person = new Object();
 person['firstname'] = 'Tony'
 person['lastName'] = 'Alicea'
 
+console.log(person.lastName);
+
+person.address = new Object();
+person.address.street = "111 Main St.";
+person.address.city = 'New Yourk';
+person.address.state = 'NY';
+
+var child = {
+    firstname: 'Alice',
+    address: {
+        street: '1 Main St.',
+        city: 'New Yourk',
+        state: 'NY'
+    }    
+};
+
+function greet(person) {
+    console.log('Hi ' + person.firstname);
+}
