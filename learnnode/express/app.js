835b9b2189d69fdfdc84e2cace661b3c7ebff589
Ivan Katliarchuk <tanzwud.work@gmail.com>
2017-10-05 20:35:31 +0100

    json parser

--- learnnode/express/app.js
+++ learnnode/express/app.js
@@ -1,9 +1,15 @@
 let express = require('express');
+let bodyParser = require('body-parser');
 let app = express();
 
 let port = process.env.PORT || 3000;
-
+let urlencodedParser = bodyParser.urlencoded({ extended: false });
+let jsonParser = bodyParser.json();
+app.use(jsonParser);
 app.use('/assets', express.static(__dirname + '/public'));
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
 app.use('/', function (req, res, next) {
 	console.log('Request Url:' + req.url);
@@ -11,11 +17,22 @@ app.use('/', function (req, res, next) {
 });
 
 app.get('/', function (reg, res) {
-    res.send('<html><head><link href=assets/style.css type=text/css rel=stylesheet /></head><body><h1>Hello world!</h1></body></html>');
+    res.render('index');
+});
+
+app.get('/person/:id', function(req, res) {
+    res.render('person', {ID: req.params.id, Qstr: req.query.qstr});
 });
 
 app.get('/api', function (reg, res) {
     res.json({ firstname: 'John', lastname: 'Doe' })
+});
+
+// curl -d '{"firstname":"John", "lastname":"Doe"}' -H "Content-Type: application/json" -X POST http://localhost:3000/person
+app.post('/person', urlencodedParser, function(req, res) {
+    res.send('Thank you!');
+    console.log(req.body.firstname);
+	console.log(req.body.lastname);
 });
 
 app.get('/person/:id', function (req, res) {
