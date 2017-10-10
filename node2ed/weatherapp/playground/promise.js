33d86de8df77f707c06dab48ed683b94485c189c
Ivan Katliarchuk <tanzwud.work@gmail.com>
2017-10-10 22:07:10 +0100

    promise advanced

--- node2ed/weatherapp/playground/promise.js
+++ node2ed/weatherapp/playground/promise.js
@@ -1,4 +1,24 @@
 'use strict';
+let asyncAdd = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (typeof a === 'number' && typeof b === 'number') {
+                resolve(a + b);
+            } else {
+                reject('Argument must be numbers');
+            }
+        }, 1500);
+    });
+};
+
+asyncAdd(3, 6).then((res) => {
+    console.log('Result: ', res);
+    return asyncAdd(res, 11);
+}).then((res) => {
+    console.log('Next Result: ', res);
+}).catch((err) => {
+    console.log('Cannot add non number');
+});
 
 let somePromise = new Promise((resolve, reject) => {
     setTimeout(() => {
