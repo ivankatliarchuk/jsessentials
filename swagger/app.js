671303571f9d433cd217635aba7e06c103adfa79
Ivan Katliarchuk <tanzwud.work@gmail.com>
2017-10-11 07:18:31 +0100

    express swagger

--- swagger/app.js
+++ swagger/app.js
@@ -1,3 +1,15 @@
-// todo 
-// Create swagger doc
+const express = require('express');
+const app = express();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
+
+var showExplorer = true;
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, showExplorer));
+
+app.use('/', (req, res) => {
+    res.send('Hello World')
+});
+
+app.listen(3000);
