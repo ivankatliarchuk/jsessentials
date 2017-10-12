c7a9f6681ad4bdede6a223f9f443b80a6e701524
Ivan Katliarchuk <tanzwud.work@gmail.com>
2017-10-12 16:50:34 +0100

    testing code

--- node2ed/basetest/utils/utils.test.js
+++ node2ed/basetest/utils/utils.test.js
@@ -1,15 +1,34 @@
+const expect = require('expect');
 const utils = require('./utils');
 
 it('should add two numbers.', () => {
     let res = utils.add(33, 11);
-    if (res !== 44) {
-        throw new Error(`Expected 44 but got ${res}`);
-    }    
+    expect(res).toBe(44).toBeA('number');
 });
 
 it('should square two numbers.', () => {
     let res = utils.square(3);
-    if (res !== 9) {
-        throw new Error(`Expected 9 but got ${res}`);
-    }
+    expect(res).toBe(9).toBeA('number');
+});
+
+it('should expect some values', () => {
+    expect(12).toNotBe(11);
+    expect({
+        name: 'Andrew',
+        age: 25,
+        location: 'Philadelphia'
+    }).toInclude({
+        age: 25
+    });
+});
+
+// should verify fist and last names are set
+it('should set firstName and lastName', () => {
+    let user = { location: 'Philadelphia', age: 25 };
+    let res = utils.setName(user, 'Andrew Mead');
+   // expect(user).toEqual(res);
+    expect(res).toInclude({
+        firstName: 'Andrew',
+        lastName: 'Mead'
+    })
 });
