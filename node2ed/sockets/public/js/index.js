f04faf0dc806e139fb9726ad3b82ace00fc533d1
Ivan Katliarchuk <tanzwud.work@gmail.com>
2017-11-07 20:33:49 +0000

    broadcast location data

--- node2ed/sockets/public/js/index.js
+++ node2ed/sockets/public/js/index.js
@@ -2,15 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connnected!!!');
-
-    // socket.emit('createEmail', {
-    //     to: 'jen@example.com',
-    //     text: 'Hey. This is Andrew'
-    // });
-    // socket.emit('createMessage', {
-    //     from: 'Andrew',
-    //     text: 'Yup, that works for me'
-    // });
 });
 
 socket.on('disconnnect', function () {
@@ -23,4 +14,33 @@ socket.on('newEmail', function (email) {
 
 socket.on('newMessage', function (message) {
     console.log('new message', message);
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}:${message.text}`);
+    jQuery('#messages').append(li);
+});
+
+
+jQuery('#message-form').on('submit', function (e) {
+    e.preventDefault();
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name=message]').val()
+    }, function () {       
+    });
+});
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function () {
+    if (!navigator.geolocation) {
+        return alert('Geolocation not supported buy your browser.');
+    }
+
+    navigator.geolocation.getCurrentPosition(function (position) {
+        socket.emit('createLocationMessage', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        });
+    }, function () {
+        alert('Unable to fetch locationn');
+    });
 });
