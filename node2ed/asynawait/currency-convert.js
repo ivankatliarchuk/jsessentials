3046d7d6877142a477ee45f8a9ce688f663dfef1
Ivan Katliarchuk <tanzwud.work@gmail.com>
2017-11-11 21:52:33 +0000

    async error handler

--- node2ed/asynawait/currency-convert.js
+++ node2ed/asynawait/currency-convert.js
@@ -7,16 +7,22 @@
 
 const axios = require('axios');
 
-const getExchangeRate = (from, to) => {
-    return axios.get(`http://api.fixer.io/latest?base=${from}`).then((response) => {
+const getExchangeRate = async (from, to) => {
+    try {
+        const response = await axios.get(`http://api.fixer.io/latest?base=${from}`);
         return response.data.rates[to];
-    });
+    } catch (e) {
+        throw new Error(`Unable to get rate ${from} ${to}.`);
+    }    
 };
 
-const getCountries = (currencyCode) => {
-    return axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`).then((response) => {        
+const getCountries = async (currencyCode) => {
+    try {
+        const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`);
         return response.data.map((country) => country.name);
-    });
+    } catch (e) {
+        throw new Error(`Unable to get countries that use ${currencyCode}.`);
+    }    
 };
 
 const convertCurrency = async (from, to, amount) => {
@@ -26,7 +32,8 @@ const convertCurrency = async (from, to, amount) => {
     return `${amount} ${from} is worth ${exchangeAmount} ${to}. ${to} can be used in following countries: ${countries.join(', ')}`;
 };
 
-
-convertCurrency('USD', 'RUB', 100).then((result) => {
+convertCurrency('USD', 'GBP', 100).then((result) => {
     console.log(result);
+}).catch((e) => {
+    console.log(e.message);
 });
