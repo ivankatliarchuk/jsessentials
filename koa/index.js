41387820a903230426ea406a7c2c3ae1f03f405b
Ivan Katliarchuk <tanzwud.work@gmail.com>
2017-12-26 15:55:57 +0000

    added linters

---
+++ koa/index.js
@@ -0,0 +1,12 @@
+"use strict";
+const log = msg => console.log(msg);
+
+log("hello");
+
+throw new Error('error!');
+
+process.on('unhandledRejection', (e) => {
+    logger.error(e);
+
+    process.exit(255);
+});
